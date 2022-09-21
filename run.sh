9266783b6cee0533ba82083c9741d58b6c7a0068
Bruno Benkel <bruno.benkel@gmail.com>
2022-09-21 16:04:36 -0300

    Added options to only install or to only run

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 usage() {
     echo ""
-    echo "usage: $0 [-hserc] [-n <nevents>] [-j <njobs>] [-y <yaml>] [-i <inputfile>] c1 [c2 ...]"
+    echo "usage: $0 [-hsercIR] [-n <nevents>] [-j <njobs>] [-y <yaml>] [-i <inputfile>] c1 [c2 ...]"
     echo "    -h             Display this usage message."
     echo "    -s             Use JVM serial garbage collector (-XX:+UseSerialGC)."
     echo "    -e             Use experimental options (-XX:+UseJVMCICompiler)."
-    echo "    -r             Run only recon-util."
-    echo "    -c             Run only clara."
+    echo "    -r             Install and/or run only recon-util."
+    echo "    -c             Install and/or run only clara."
+    echo "    -I             Only install recon-util and/or clara, do not run."
+    echo "    -R             Only run recon-util and/or clara from a previous installation."
     echo "    -n <nevents>   Number of events per job. Default is 10.000."
     echo "    -j <njobs>     Number of parallel jobs per version. Default is 1."
     echo "    -y <yaml>      Absolute path to yaml file used for the test. Default is "
@@ -24,7 +26,7 @@ usage() {
 
 # --+ HANDLE ARGS +---------------------------------------------------------------------------------
 # Get args.
-while getopts "hsercn:j:y:i:" opt; do
+while getopts "hsercIRn:j:y:i:" opt; do
     case "${opt}" in
         \? ) usage;;
         h  ) usage;;
@@ -32,6 +34,8 @@ while getopts "hsercn:j:y:i:" opt; do
         e  ) TESTOPTS=$TESTOPTS' -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler';;
         r  ) ONLYRECONUTIL=true;;
         c  ) ONLYCLARA=true;;
+        I  ) ONLYINSTALL=true;;
+        R  ) ONLYRUN=true;;
         n  ) NEVENTS=${OPTARG};;
         j  ) NJOBS=${OPTARG};;
         y  ) YAML=${OPTARG};;
@@ -43,6 +47,8 @@ shift $((OPTIND -1))
 # Give optargs a default value if none is given.
 if [ ! -n "$ONLYRECONUTIL" ]; then ONLYRECONUTIL=false;       fi
 if [ ! -n "$ONLYCLARA" ];     then ONLYCLARA=false;           fi
+if [ ! -n "$ONLYINSTALL" ];   then ONLYINSTALL=false;         fi
+if [ ! -n "$ONLYRUN" ];       then ONLYRUN=false;             fi
 if [ ! -n "$NEVENTS" ];       then NEVENTS=10000;             fi
 if [ ! -n "$NJOBS" ];         then NJOBS=1;                   fi
 if [ ! -n "$YAML" ];          then YAML="$PWD/yaml/all.yaml"; fi
@@ -51,6 +57,10 @@ if [ ! -n "$INPUTFILE" ];     then INPUTFILE="...";           fi # TODO. Add a f
 # Check args.
 if [ "$ONLYRECONUTIL" = true ] && [ "$ONLYCLARA" = true ]; then
     echo "-r and -c are not compatible!"
+    usage
+fi
+if [ "$ONLYINSTALL" = true ] && [ "$ONLYRUN" = true ]; then
+    echo "-I and -R are not compatible!"
     usage
 fi
 if [ "$NEVENTS" -le 0 ]; then echo "Number of events can't be 0 or negative!"; usage; fi
@@ -69,91 +79,107 @@ CLARADIR="$PWD/clara"
 JUNKDIR="$PWD/junk"
 LOGDIR="$PWD/log"
 
-# Clear out $INDIR, $OUTDIR, $LOGDIR, and $CLARADIR.
-rm $INDIR/*.hipo    2> /dev/null
-rm $INDIR/*.txt     2> /dev/null
-rm $OUTDIR/*.hipo   2> /dev/null
-rm $LOGDIR/*.txt    2> /dev/null
-rm -rf $CLARADIR/*/ 2> /dev/null
+if [ $ONLYRUN = false ]; then
+    # Clear out $INDIR.
+    rm $INDIR/*.hipo    2> /dev/null
+    rm $INDIR/*.txt     2> /dev/null
 
-# Copy file to $INDIR and reduce to NEVENTS to minimize disk usage.
-echo ""
-echo "Copying input file to $INDIR."
-# TODO. Add banks needed by CVT.
-TMPFILE="$INDIR/tmp.hipo"
-hipo-utils -filter -b "RUN::config,DC::tdc" -n $NEVENTS -o $TMPFILE $INPUTFILE > /dev/null
+    # Copy file to $INDIR and reduce to NEVENTS to minimize disk usage.
+    echo ""
+    echo "Copying input file to $INDIR."
+    # TODO. Add banks needed by CVT.
+    TMPFILE="$INDIR/tmp.hipo"
+    hipo-utils -filter -b "RUN::config,DC::tdc" -n $NEVENTS -o $TMPFILE $INPUTFILE > /dev/null
 
-# Copy input file and install clara.
-echo "Setting up running environment (this can take a while)."
-for ((JOB=0;JOB<$NJOBS;++JOB)); do
-    for CLAS12VER in "${CLAS12VERS[@]}"; do
-        # Get CLAS12 recon version filename.
-        IFS='/' read -ra ADDR <<< "$CLAS12VER"
-        for i in "${ADDR[@]}"; do RECONNAME=$i; done # Dirty but gets the job done.
+    # Clear out $CLARADIR.
+    rm -rf $CLARADIR/*/ 2> /dev/null
 
-        # --+ recon-util +--------------------------------------------------------------------------
-        if [ $ONLYCLARA = false ]; then
-            RUNNAME="$RECONNAME.reconutil-$JOB"
-            cp "$TMPFILE" "$INDIR/$RUNNAME.hipo" # Copy input file.
-        fi
+    # Copy input file and install clara.
+    echo "Setting up running environment (this can take a while)."
+    for ((JOB=0;JOB<$NJOBS;++JOB)); do
+        for CLAS12VER in "${CLAS12VERS[@]}"; do
+            # Get CLAS12 recon version filename.
+            IFS='/' read -ra ADDR <<< "$CLAS12VER"
+            for i in "${ADDR[@]}"; do RECONNAME=$i; done # Dirty but gets the job done.
 
-        # --+ clara +-------------------------------------------------------------------------------
-        if [ $ONLYRECONUTIL = false ]; then
-            RUNNAME="$RECONNAME.clara-$JOB"
-            cp "$TMPFILE" "$INDIR/$RUNNAME.hipo" # Copy input file.
-            echo "$RUNNAME.hipo" > "$INDIR/$RUNNAME.txt"
+            # --+ recon-util +--------------------------------------------------------------------------
+            if [ $ONLYCLARA = false ]; then
+                RUNNAME="$RECONNAME.reconutil-$JOB"
+                cp "$TMPFILE" "$INDIR/$RUNNAME.hipo" # Copy input file.
+            fi
 
-            # Install clara from $CLAS12VER.
-            echo "  * Installing clara for $RUNNAME."
-            cd "$CLARADIR"
-            tar -C "$CLAS12VER" -czf "$CLARADIR/coatjava-$RUNNAME.tar.gz" "coatjava"
-            export CLARA_HOME="$CLARADIR/$RUNNAME"
-            ./install-claracre-clas.sh "$RUNNAME"
-            rm "coatjava-$RUNNAME.tar.gz"
-            cd - > /dev/null
-        fi
+            # --+ clara +-------------------------------------------------------------------------------
+            if [ $ONLYRECONUTIL = false ]; then
+                RUNNAME="$RECONNAME.clara-$JOB"
+                cp "$TMPFILE" "$INDIR/$RUNNAME.hipo" # Copy input file.
+                echo "$RUNNAME.hipo" > "$INDIR/$RUNNAME.txt"
+
+                # Install clara from $CLAS12VER.
+                echo "  * Installing clara for $RUNNAME."
+                cd "$CLARADIR"
+                tar -C "$CLAS12VER" -czf "$CLARADIR/coatjava-$RUNNAME.tar.gz" "coatjava"
+                export CLARA_HOME="$CLARADIR/$RUNNAME"
+                ./install-claracre-clas.sh "$RUNNAME"
+                rm "coatjava-$RUNNAME.tar.gz"
+                cd - > /dev/null
+            fi
+        done
     done
-done
-rm $TMPFILE
+    rm $TMPFILE
+fi
 
 # --+ RUN +-----------------------------------------------------------------------------------------
-for ((JOB=0;JOB<$NJOBS;++JOB)); do
-    for CLAS12VER in "${CLAS12VERS[@]}"; do
-        # Get CLAS12 recon version filename.
-        IFS='/' read -ra ADDR <<< "$CLAS12VER"
-        for i in "${ADDR[@]}"; do RECONNAME=$i; done # Dirty but gets the job done.
-        # --+ recon-util +--------------------------------------------------------------------------
-        if [ $ONLYCLARA = false ]; then
-            RUNNAME="$RECONNAME.reconutil-$JOB"
+if [ $ONLYINSTALL = false ]; then
+    # Clear out $OUTDIR and $LOGDIR.
+    rm $OUTDIR/*.hipo   2> /dev/null
+    rm $LOGDIR/*.txt    2> /dev/null
 
-            export LOGDIR=$LOGDIR
-            export RUNNAME=$RUNNAME
-            export CLAS12VER="$CLAS12VER"
-            export JAVA_OPTS="$TESTOPTS -Xmx1536m -Xms1024m"
+    for ((JOB=0;JOB<$NJOBS;++JOB)); do
+        for CLAS12VER in "${CLAS12VERS[@]}"; do
+            # Get CLAS12 recon version filename.
+            IFS='/' read -ra ADDR <<< "$CLAS12VER"
+            for i in "${ADDR[@]}"; do RECONNAME=$i; done # Dirty but gets the job done.
+            # --+ recon-util +--------------------------------------------------------------------------
+            if [ $ONLYCLARA = false ]; then
+                RUNNAME="$RECONNAME.reconutil-$JOB"
 
-            # Run.
-            ./call-recon-util.sh \
-                    -i "$INDIR/$RUNNAME.hipo" -o "$OUTDIR/$RUNNAME.hipo" -y $YAML -n $NEVENTS
-        fi
+                export LOGDIR=$LOGDIR
+                export RUNNAME=$RUNNAME
+                export CLAS12VER="$CLAS12VER"
+                export JAVA_OPTS="$TESTOPTS -Xmx1536m -Xms1024m"
 
-        # --+ clara +-------------------------------------------------------------------------------
-        if [ $ONLYRECONUTIL = false ]; then
-            RUNNAME="$RECONNAME.clara-$JOB"
-            # ulimit -u 49152
-            export CLARA_HOME="$CLARADIR/$RUNNAME"
-            export CLARA_USER_DATA="$JUNKDIR"
-            export CLAS12DIR="$CLARA_HOME/plugins/clas12"
-            unset CLARA_MONITOR_FE
-            export PATH=${PATH}:$CLAS12DIR/bin
-            export JAVA_OPTS="$TESTOPTS -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties"
+                # Run.
+                ./call-recon-util.sh \
+                        -i "$INDIR/$RUNNAME.hipo" -o "$OUTDIR/$RUNNAME.hipo" -y $YAML -n $NEVENTS
+            fi
 
-            # Run.
-            bash $CLARA_HOME/lib/clara/run-clara \
-                    -i $INDIR -o $OUTDIR -z "out_" -x $JUNKDIR -t 1 -e $NEVENTS -s $RUNNAME \
-                    $YAML "$INDIR/$RUNNAME.txt" > "$LOGDIR/$RUNNAME.txt" &
-            sleep 5
-        fi
+            # --+ clara +-------------------------------------------------------------------------------
+            if [ $ONLYRECONUTIL = false ]; then
+                RUNNAME="$RECONNAME.clara-$JOB"
+                # ulimit -u 49152
+                export CLARA_HOME="$CLARADIR/$RUNNAME"
+                export CLARA_USER_DATA="$JUNKDIR"
+                export CLAS12DIR="$CLARA_HOME/plugins/clas12"
+                export PATH=${PATH}:$CLAS12DIR/bin
+                unset CLARA_MONITOR_FE
+
+                if [ -z $CCDB_CONNECTION ] || ! [[ $CCDB_CONNECTION = sqlite* ]]; then
+                    export CCDB_CONNECTION=mysql://clas12reader@clasdb-farm.jlab.org/clas12
+                fi
+                export RCDB_CONNECTION=mysql://rcdb@clasdb-farm.jlab.org/rcdb
+
+                v=`$CLARA_HOME/lib/clara/run-java -version 2>&1 | head -1 | awk '{print$3}' | sed 's/"//g' | awk -F\. '{print$1}'`
+                if [ $v -ge 11 ]; then JAVA_OPTS="$JAVA_OPTS $TESTOPTS"; fi
+                JAVA_OPTS="$JAVA_OPTS -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties"
+
+                # Run.
+                bash $CLARA_HOME/lib/clara/run-clara \
+                        -i $INDIR -o $OUTDIR -z "out_" -x $JUNKDIR -t 1 -e $NEVENTS -s $RUNNAME \
+                        $YAML "$INDIR/$RUNNAME.txt" > "$LOGDIR/$RUNNAME.txt" &
+                sleep 5
+            fi
+        done
     done
-done
+fi
 
 # TODO. Write output to well-formatted file?
